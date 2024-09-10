32189d709d59622109cc4e9078d6d5f5cab3083b
Guiga189 <guilhermeguinalli@gmail.com>
2024-09-10 11:08:35 -0300

    Alura

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo : "Teatro Elis Regina",
+        descrição : "Teatro Elis Regina: Um tributo à Rainha da Música Popular Brasileira em Porto Alegre.O Teatro Elis Regina é um espaço cultural localizado em Porto Alegre, Rio Grande do Sul, dedicado à memória e ao legado da icônica cantora brasileira. A escolha do nome é uma homenagem justa à artista que marcou a história da música popular brasileira, sendo considerada por muitos a maior cantora do país.",
+        link : "https://pt.wikipedia.org/wiki/Elis_Regina",
+        link : "https://www.youtube.com/watch?v=ew_p1IRRgks",
+    },
+    {
+        titulo : "Teatro São Pedro",
+        descrição : "O Teatro São Pedro é um dos mais importantes teatros de Porto Alegre, com uma rica história e programação variada.",
+        link : "https://pt.wikipedia.org/wiki/Theatro_S%C3%A3o_Pedro_(Porto_Alegre)",
+    },
+    {
+      titulo :  "Teatro de Arena",
+      descrição : "O Teatro de Arena é um espaço cultural ao ar livre, localizado em um local histórico de Porto Alegre, e oferece uma programação variada de eventos e espetáculos.",
+      link : "https://pt.wikipedia.org/wiki/Teatro_de_Arena_(Porto_Alegre)"
+    },
+    {
+        titulo : "Teatro Túlio Piva",
+        descrição : "O Teatro de Câmara foi criado em 1970 como o primeiro teatro administrado pela municipalidade, sendo instalado provisoriamente no prédio de um antigo depósito de automóveis, que futuramente deveria ser demolido para dar lugar à abertura de uma avenida. Com a alteração no Plano Diretor da cidade o teatro acabou permanecendo ali definitivamente, sendo reformado e reinaugurado em 1999 já com sua denominação atual, que homenageia o conhecido músico gaúcho Túlio Piva. Novamente reformado em 2005 com a instalação de diversos melhoramentos, foi reaberto ao público em março de 2006.",
+        link : "https://pt.wikipedia.org/wiki/Teatro_de_C%C3%A2mara_T%C3%BAlio_Piva",
+    },
+];
+
